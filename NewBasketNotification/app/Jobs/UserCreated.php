d90772233633333e9747859fce01621829aa055c
achawayne3031 <59153600+achawayne3031@users.noreply.github.com>
2024-02-28 10:38:07 +0100

    update

---
+++ NewBasketNotification/app/Jobs/UserCreated.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Notifications;
+use Illuminate\Support\Facades\Log;
+
+class UserCreated implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $data;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($data)
+    {
+        //
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        //
+
+        Log::info($this->data);
+
+        Notifications::create([
+            'name' => $this->data['first_name'],
+            'email' => $this->data['email'],
+        ]);
+    }
+}
---
+++ NewBasketService/app/Http/Controllers/UserController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    protected User $model;
+
+    public function __construct(User $model)
+    {
+        $this->model = $model;
+    }
+
+    public function store(Request $request)
+    {
+        $request = $request->all();
+        $user = $this->model->create($request);
+        return response()->json([
+            'data' => $user,
+            'message' => 'User added successfully',
+        ]);
+    }
+
+    public function index()
+    {
+        $users = $this->model->all();
+        return response()->json([
+            'data' => $users,
+            'message' => 'All users',
+        ]);
+    }
+
+    public function show(User $user)
+    {
+        return response()->json([
+            'data' => $user,
+        ]);
+    }
+
+    public function destory(User $user)
+    {
+        $user->delete();
+        return response->json([
+            'message' => 'User deleted successfully',
+        ]);
+    }
+}

312e568c2bd40376e27790b51cf1c9a814277c70
achawayne3031 <59153600+achawayne3031@users.noreply.github.com>
2024-03-03 20:35:05 +0100

    update

--- NewBasketService/routes/api.php
+++ NewBasketService/routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/user', [UserController::class, 'store']);
+Route::apiResource('/user', UserController::class);
